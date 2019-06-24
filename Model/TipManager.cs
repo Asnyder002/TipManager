cfa13eb99aab033fe63fd6ea428b8df6de9ceae2
Adam Snyder <adam296@live.com>
2019-06-23 19:45:06 -0500

    Initial Commit

---
+++ Model/TipManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipManager.Model
+{
+    public class TipManager
+    {
+        public double Total { get; set; }
+        public double TotalMade { get; set; }
+        public double TotalSpent { get; set; }
+        public double TotalHours { get; set; }
+    }
+}
