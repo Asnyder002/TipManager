18e337b87d0ff7c2f15155e8097d7753bf1e94db
Adam Snyder <adam296@live.com>
2019-06-24 23:35:13 -0500

    Starting on service layer repository

---
+++ Services/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipManager.Services
+{
+    class Repository
+    {
+        public double GetSumOfDeposits()
+        {
+            using(var context = new TipManagerDBEntities())
+            {
+                double sum = (double)context.Deposits.Sum(d => d.DepositAmount);
+                return sum;
+            }
+        }
+    }
+}
