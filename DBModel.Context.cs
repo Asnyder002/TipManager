f149646491d804a67dd26c2d745f1bd1df2d26e3
Adam Snyder <adam296@live.com>
2019-07-06 02:30:13 -0500

    Fixed connection string issue by passing connection directly to context class and deleting connection from app.config

--- DBModel.Context.cs
+++ DBModel.Context.cs
@@ -16,7 +16,7 @@ namespace TipManager
     public partial class TipManagerDBEntities : DbContext
     {
         public TipManagerDBEntities()
-            : base("name=TipManagerDBEntities")
+            : base(@"metadata=res://*/DBModel.csdl|res://*/DBModel.ssdl|res://*/DBModel.msl;provider=System.Data.SqlClient;provider connection string=';data source=DESKTOP-GNG12RP\SQLEXPRESS;initial catalog=TipManagerDB;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework';")
         {
         }
     
--- Presenter/TipManagerPresenter.cs
+++ Presenter/TipManagerPresenter.cs
@@ -13,42 +13,6 @@ namespace TipManager.Presenter
 {
     class TipManagerPresenter
     {
-        // Creates object from model
-        TipManagerModel tipManager = new TipManagerModel();
-
-        TipManagerServices services;
-
-        // Creates instance from interface
-        private ITipManager tipManagerView;
-
-        // Constructor
-        public TipManagerPresenter(ITipManager view)
-        {
-            tipManagerView = view;
-            services = new TipManagerServices(tipManager);
-        }
-
-        public void DisplayTotal()
-        {
-            services.passSumTotalToModel();
-            tipManagerView.TxtTotal = "$" + tipManager.Total.ToString();
-            
-        }
-
-        public void DisplayTotalMade()
-        {
-            tipManagerView.TxtTotalMade = tipManager.TotalMade.ToString();
-        }
-
-        public void DisplayTotalSpent()
-        {
-            tipManagerView.TxtTotalSpent = tipManager.TotalSpent.ToString();
-        }
-
-        public void DisplayTotalHours()
-        {
-            tipManagerView.TxtTotalHours = tipManager.TotalHours.ToString();
-        }
-
+       
     }
 }
